00d20181410731cd96120309bb072154ab3f6d79
Khaleefa <ktkhaleefakt@gmail.com>
2019-03-22 11:52:08 +0530

    create new function for find neighbours

--- game_of_life.py
+++ game_of_life.py
@@ -1,13 +1,5 @@
-#  1.  Any live cell with fewer than two live neighbors dies, as if by underpopulation.
-#  2.  Any live cell with two or three live neighbors lives on to the next generation.
-#  3.  Any live cell with more than three live neighbors dies, as if by overpopulation.
-#  4.  Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
 import time
 
-def board(size):
-    """Create a 3*3 matrix """
-    board=[[0,1,0],[0,1,0],[0,1,0]]
-    return board
 
 def cell_alive_check(cell):
     return cell  == 1
@@ -23,51 +15,64 @@ def neighbour_check(board,row,col):
                 continue
             if next_row <0 or next_col<0 or next_row >size_limit or next_col >size_limit:
                 continue
-            if cell_alive_check(board[next_row][next_col]):
+            if board[next_row][next_col]==1:
                 count = count + 1
     return count
 
-def set_rule(board, neighbour):
-    size =3
-    for row in range(size):
-        for col in range(size):
-            if cell_alive_check(board[row][col]):
-                if neighbour[row][col] < 2:
-                    board[row][col] = 0
-                if neighbour[row][col] == 2 or neighbour[row][col] == 3:
-                    board[row][col] = 1
-                if neighbour[row][col] > 3:
-                    board[row][col] = 0
+#  1.  Any live cell with fewer than two live neighbors dies, as if by underpopulation.
+#  2.  Any live cell with two or three live neighbors lives on to the next generation.
+#  3.  Any live cell with more than three live neighbors dies, as if by overpopulation.
+#  4.  Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
+
+def set_rule(board):
+    new_board=[[0,0,0],
+               [0,0,0],
+               [0,0,0]]
+    row_size = len(board)
+    col_size = len(board)
+    for row in range(row_size):
+        for col in range(col_size):
+            if neighbour_check(board,row,col) < 2 and board[row][col] == 1:
+                new_board[row][col] = 0
+            elif neighbour_check(board,row,col) in [2,3] and board[row][col] == 1:
+                new_board[row][col] = 1
+            elif neighbour_check(board,row,col) > 3 and board[row][col] == 1:
+                new_board[row][col]= 0
             else:
-                if neighbour[row][col] == 3:
-                    board[row][col] = 1
+                if neighbour_check(board,row,col) == 3 and board[row][row] == 0:
+                    new_board[row][col] =1
 
-    return board
-
-def 
-
+    return new_board
 
 def display(board):
-    size = size(baord)
-    for row in range(size):
-        for col in range(size):
-            if cell_alive_check(board[row][col]):
-                print(" 1 ", end=' ')
+    size = len(board)
+    rows = []
+    for i in range(size):
+        cols = []
+        for j in range(size):
+            if board[i][j] == 1:
+                cols.append("1")
             else:
-                print(" 0 ", end=' ')
-        print("\n")
+                cols.append("0")
+        rows.append(" ".join(cols))
+    return "\n\n".join(rows)
 
 def main(board):
-    board = [[0,1,0],[0,1,0],[0,1,0]]
-    for i in range(10):
-        print(f"\n{i} Generation")
-        time.sleep(1)
-        display(board)
-        a=  set_rule(board,neighbour_check(board,row,col))
-    return a 
+    row = len(board)
+    col =len(board)
+    for i in range(0,10):
+        print("\n{} Generation".format(i))
+        time.sleep(0.5)
+        print(display(board))
+        board = set_rule(board)
+    time.sleep(0.5)
     
     
 if __name__ == "__main__":
+    board = [[0,1,0],
+             [0,1,0],
+             [0,1,0]]
+    
     main(board)
         
 
